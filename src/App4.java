e19be12392ace96a720e53181143f6de467a2f06
kaufmantw <kaufmantw@jacks.sfasu.edu>
2022-11-03 13:01:29 -0500

    step 1
    step 1 done

--- src/App4.java
+++ src/App4.java
@@ -1,4 +1,6 @@
 import fruitNinjaHelper.cs331FruitNinjaFrame;
+import fruitNinjaHelper.cs331FruitNinjaGame;
+import fruitNinjaHelper.cs331ScoreController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,6 +10,8 @@ public class App4 extends Application{
         cs331FruitNinjaFrame frame = new cs331FruitNinjaFrame(primaryStage);
         
         // Your code goes here!!
+        frame.addGame(new Game());
+        frame.setTop(new cs331ScoreController());
         
     }
     public static void main(String[] args) throws Exception {
