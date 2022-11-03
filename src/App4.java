3073bb8113852721e81acbc1d0c2334b6b1e95b3
kaufmantw <kaufmantw@jacks.sfasu.edu>
2022-11-03 12:56:55 -0500

    setup
    initial commit setting up the project environment

---
+++ src/App4.java
@@ -0,0 +1,16 @@
+import fruitNinjaHelper.cs331FruitNinjaFrame;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App4 extends Application{
+
+    public void start(Stage primaryStage) {
+        cs331FruitNinjaFrame frame = new cs331FruitNinjaFrame(primaryStage);
+        
+        // Your code goes here!!
+        
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import fruitNinjaHelper.cs331FruitNinjaGame;
+
+public class Game extends cs331FruitNinjaGame{
+
+    // You may need to add constructor
+
+    
+    public void updateChoppable(Object object) {
+        
+        // Your code goes here
+        
+    }
+
+    @Override
+    public Object launchItem() {
+
+        // YOUR code goes here
+        return null;
+    }
+
+    // Do not modify anything below this line
+    @Override
+    public void updateChoppableHelper() {
+        this.updateChoppable(this.getCurrentItem());
+    }
+}
